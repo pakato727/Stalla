9506082fa186abfa62a4ace7bb2d6fa71ded98a4
Pakato727 <146851784+Pakato727@users.noreply.github.com>
2024-05-17 20:40:52 +0200

    master

---
+++ src/Avvio.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Avvio {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		System.out.println("********MENU********");
+		System.out.println("1 - Aggiungi animale ");
+		System.out.println("2 - Visualizza singolo animale ");
+		System.out.println("3 - Esci.");
+		System.out.println("********************");
+		
+		GestoreStalla stalla = new GestoreStalla();
+		Scanner input = new Scanner(System.in);
+		
+		int scelta = 1;
+		
+		
+		do {
+			System.out.println("Scegli opzione: ");
+			scelta=Integer.parseInt(input.nextLine());
+			switch(scelta) {
+			case 1:
+				stalla.aggiungiAnimali();
+				System.out.println("Premi un tasto per continuare: ");
+				input.nextLine();
+				break;
+				
+			case 2:
+				System.out.println("Inserisci ID animale da visualizzare: ");
+				int id = Integer.parseInt(input.nextLine());
+				stalla.leggiAnimale(id);
+				System.out.println("Premi un tasto per continuare: ");
+				input.nextLine();
+				break;
+			
+			case 3:
+				System.out.println("Grazie e arrivederci!");
+				break;
+			}
+			
+		}
+		while(scelta != 5);
+
+	
+
+	}
+
+}
---
+++ src/GestoreStalla.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class GestoreStalla {
+	String[] stalla = new String[5];
+	Scanner input = new Scanner(System.in);
+
+	void aggiungiAnimali() {
+		boolean flag=true;
+
+
+		for(int i=0; i<stalla.length; i++) {
+			if(stalla[i] == null){
+				System.out.println("Inserisci ID: ");
+				int id = Integer.parseInt(input.nextLine());
+				System.out.println("Tipo animale: ");
+				String tipoAnimale = input.nextLine();
+				System.out.println("Provenienza: ");
+				String provenienza = input.nextLine();
+				System.out.println("Tipo alimentazione: ");
+				String tipoAlimentazione = input.nextLine();
+				System.out.println("Valore: ");
+				Double valore = Double.parseDouble(input.nextLine());
+				String animale = id+";"+tipoAnimale+";"+provenienza+";"+tipoAlimentazione+";"+valore;
+				stalla[i] = animale;
+				flag=false;
+				break;
+			}
+
+		}
+
+
+
+		if(flag) {
+			System.out.println("Stalla satura! Non è possibile aggiungere animali");
+		}
+
+
+
+	}
+
+
+
+	void leggiAnimale(int id) {
+		//System.out.println("Inserisci ID animale da visualizzare: ");
+		//		id = Integer.parseInt(input.nextLine());
+
+		for(int i=0; i<stalla.length; i++) {
+			String[] campi = stalla[i].split(";");
+			int idCorrente = Integer.parseInt(campi[0]);
+			if(id == idCorrente) {
+				System.out.println("**SCHEDA ANIMALE**");
+				System.out.println("ID: "+ campi[0]);
+				System.out.println("Tipo di animale: "+campi[1]);
+				System.out.println("Provenienza: "+campi[2]);
+				System.out.println("Alimentazione: "+campi[3]);
+				System.out.println("Valore: "+campi[4]);
+				System.out.println("*******");
+				break;
+
+			}
+			else {
+				System.out.println("ID non esistente");
+
+			}
+
+		}
+
+
+
+
+
+
+
+	}
+
+	
+}
